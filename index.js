ef6dd68f72eddb4952c26681dc8df2ed7dee1d6d
Ranie Santos <ranietots32@gmail.com>
2018-01-14 01:20:06 +0800

    Added config

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true
+    },
+
+    extends: [
+        'standard',
+        'plugin:vue/strongly-recommended'
+    ],
+
+    rules: {
+        // Require semicolons
+        'semi': ['error', 'always'],
+
+        // Require 4 spaces for indents
+        'indent': ['error', 4],
+
+        // Require parentheses around arrow function arguments
+        'arrow-parens': ['error', 'always'],
+
+        // Require spacing inside of braces
+        'standard/object-curly-even-spacing': ['error', 'always'],
+
+        // Overrides for eslint-plugin-vue
+        'vue/html-indent': ['error', 4, { 'alignAttributesVertically': false }],
+        'vue/max-attributes-per-line': ['off']
+    }
+}
